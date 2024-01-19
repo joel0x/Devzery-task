58bc547addc12758cae4560a3576feec3701bfcd
joel0x <joelmachado649@gmail.com>
2024-01-20 02:04:53 +0530

    inital setup

---
+++ app/components/LoginForm.jsx
@@ -0,0 +1,11 @@
+"use client";
+
+import Link from "next/link";
+export default function LoginForm() {
+
+  return (
+    <div>
+    <h1 class="text-black">login form</h1>
+    </div>
+  );
+}
---
+++ app/register/page.jsx
@@ -0,0 +1,7 @@
+import RegisterForm from "@/components/RegisterForm";
+
+export default async function Register() {
+  
+
+  return <RegisterForm />;
+}
