b16e3dc9694b378066e6bd5d305d3dd41698a91b
joel0x <joelmachado649@gmail.com>
2024-01-20 11:05:29 +0530

    resolved components

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,16 @@
-import LoginForm from "@/app/components/LoginForm"
+import LoginForm from "@/components/LoginForm";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { authOptions } from "./api/auth/[...nextauth]/route";
 
-export default function Home() {
-  return 
+export default async function Home() {
+  const session = await getServerSession(authOptions);
+
+  if (session) redirect("/dashboard");
+
+  return (
     <main>
-    <LoginForm /> 
+      <LoginForm />
     </main>
-  
+  );
 }
